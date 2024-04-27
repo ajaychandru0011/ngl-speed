0ea26189e8ac70fc54cd1e51788e1a3999ee6a2e
Nirmal-MultiVariate <nirmal@multivariate.tech>
2024-04-27 17:38:02 +0530

    height issue fixed in tools card

--- components/cards/ToolsCard.js
+++ components/cards/ToolsCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const ToolsCard = (props) => {
   return (
     <div className="col-lg-4 col-md-6 col-sm-6">
-      <div className="card-offer hover-up">
+      <div className="card-offer hover-up min-height-360">
         <div className="card-image">{props.img}</div>
         <div className="card-info">
           <h4 className="color-brand-1 mb-15">{props.name}</h4>
