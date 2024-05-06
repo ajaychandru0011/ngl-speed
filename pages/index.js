ef613b1e373ab3e53094e90b3066f8b29b1e43a1
ajaychandru0011 <ajaychandru@nextaso.com>
2024-05-06 17:28:19 +0530

    dynamic loading

--- pages/index.js
+++ pages/index.js
@@ -2,58 +2,37 @@ import Hero from "../components/home/Hero";
 
 import Layout from "../components/layout/Layout";
 import PageHead from "../components/elements/PageHead";
-import React, { Suspense, lazy } from "react";
+import dynamic from "next/dynamic";
 
 export default function Home() {
-  const DynamicTrustby = lazy(() => import("../components/home/Trustby"));
+  const DynamicTrustby = dynamic(() => import("../components/home/Trustby"));
 
-  const DynamicOurServices = lazy(() =>
+  const DynamicOurServices = dynamic(() =>
     import("../components/home/OurServices")
   );
-  const DynamicTestimonials = lazy(() =>
+  const DynamicTestimonials = dynamic(() =>
     import("../components/home/Testimonials")
   );
-  const DynamicTools = lazy(() => import("../components/home/Tools"));
-  const DynamicServices = lazy(() => import("../components/home/Services"));
-  const DynamicMarketing = lazy(() => import("../components/home/Marketing"));
-  const DynamicContact = lazy(() => import("../components/home/Contact"));
+  const DynamicTools = dynamic(() => import("../components/home/Tools"));
+  const DynamicServices = dynamic(() => import("../components/home/Services"));
+  const DynamicMarketing = dynamic(() =>
+    import("../components/home/Marketing")
+  );
+  const DynamicContact = dynamic(() => import("../components/home/Contact"));
   return (
     <>
       <PageHead
         title={"NextGrowthLabs - Best App Store Optimization Platform"}
       />
       <Layout>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Hero />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <DynamicTrustby />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicOurServices />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicTestimonials />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicTools />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicServices />
-        </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicMarketing />
-        </Suspense>
-
-        <Suspense fallback={<div>Loading...</div>}>
-          {" "}
-          <DynamicContact />
-        </Suspense>
+        <Hero />
+        <DynamicTrustby />
+        <DynamicOurServices />
+        <DynamicTestimonials />
+        <DynamicTools />
+        <DynamicServices />
+        <DynamicMarketing />
+        <DynamicContact />
       </Layout>
     </>
   );
