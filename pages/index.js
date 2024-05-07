eac095805725ed9f478f2b33b0156ef68f5cdf84
ajaychandru0011 <ajaychandru@nextaso.com>
2024-05-07 10:48:56 +0530

    removed dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,57 @@
-import Hero from "../components/home/Hero";
+// import Hero from "../components/home/Hero";
 
+// import Layout from "../components/layout/Layout";
+// import PageHead from "../components/elements/PageHead";
+// import dynamic from "next/dynamic";
+// import Script from "next/script";
+
+// export default function Home() {
+//   const DynamicTrustby = dynamic(() => import("../components/home/Trustby"));
+
+//   const DynamicOurServices = dynamic(() =>
+//     import("../components/home/OurServices")
+//   );
+//   const DynamicTestimonials = dynamic(() =>
+//     import("../components/home/Testimonials")
+//   );
+//   const DynamicTools = dynamic(() => import("../components/home/Tools"));
+//   const DynamicServices = dynamic(() => import("../components/home/Services"));
+//   const DynamicMarketing = dynamic(() =>
+//     import("../components/home/Marketing")
+//   );
+//   const DynamicContact = dynamic(() => import("../components/home/Contact"));
+//   const DynamicFooter = dynamic(() => import("../components/layout/Footer"));
+
+//   return (
+//     <>
+//       <PageHead
+//         title={"NextGrowthLabs - Best App Store Optimization Platform"}
+//       />
+//       <Layout>
+//         <Hero />
+//         <DynamicTrustby />
+//         <DynamicOurServices />
+//         <DynamicTestimonials />
+//         <DynamicTools />
+//         <DynamicServices />
+//         <DynamicMarketing />
+//         <DynamicContact />
+//         <DynamicFooter />
+//       </Layout>
+//     </>
+//   );
+// }
+import Hero from "../components/home/Hero";
+import Marketing from "../components/home/Marketing";
+import OurServices from "../components/home/OurServices";
+import Services from "../components/home/Services";
+import Testimonials from "../components/home/Testimonials";
+import Tools from "../components/home/Tools";
+import Trustby from "../components/home/Trustby";
+import Contact from "../components/home/Contact";
 import Layout from "../components/layout/Layout";
 import PageHead from "../components/elements/PageHead";
-import dynamic from "next/dynamic";
-import Script from "next/script";
-
 export default function Home() {
-  const DynamicTrustby = dynamic(() => import("../components/home/Trustby"));
-
-  const DynamicOurServices = dynamic(() =>
-    import("../components/home/OurServices")
-  );
-  const DynamicTestimonials = dynamic(() =>
-    import("../components/home/Testimonials")
-  );
-  const DynamicTools = dynamic(() => import("../components/home/Tools"));
-  const DynamicServices = dynamic(() => import("../components/home/Services"));
-  const DynamicMarketing = dynamic(() =>
-    import("../components/home/Marketing")
-  );
-  const DynamicContact = dynamic(() => import("../components/home/Contact"));
-  const DynamicFooter = dynamic(() => import("../components/layout/Footer"));
-
   return (
     <>
       <PageHead
@@ -29,14 +59,13 @@ export default function Home() {
       />
       <Layout>
         <Hero />
-        <DynamicTrustby />
-        <DynamicOurServices />
-        <DynamicTestimonials />
-        <DynamicTools />
-        <DynamicServices />
-        <DynamicMarketing />
-        <DynamicContact />
-        <DynamicFooter />
+        <Trustby />
+        <OurServices />
+        <Testimonials />
+        <Tools />
+        <Services />
+        <Marketing />
+        <Contact />
       </Layout>
     </>
   );
