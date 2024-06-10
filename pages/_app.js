7999439060bfbff9aa0166d31d8549eb8d696f23
Nirmal-MultiVariate <nirmal@multivariate.tech>
2024-06-10 18:30:14 +0530

    added GTM and meta tags.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import "../public/assets/css/style.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-538RX7C" />
     </>
   );
 }
